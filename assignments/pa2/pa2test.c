1694cd58a76c081a4bc602e5ce395a403bb71750
eth4n-e <etep9497@colorado.edu>
2025-09-18 17:30:10 -0600

    updates to pa2test.c - closer to completion

--- assignments/pa2/pa2test.c
+++ assignments/pa2/pa2test.c
@@ -4,13 +4,56 @@
 #include <sys/types.h>
 #include <fcntl.h> // for O_RDWR
 #include <errno.h> // for errno
+#include <string.h>
 // Macros
 #define READ_WRITE_MODE "r+"
-#define READ_BUF_SIZE 65536
-#define WRITE_BUF_SIZE 4096
-#define MIN(a,b) ((a) < (b) ? (a) : (b)) // "min function in c" in google
+#define READ_BUF_SIZE 4096 
+#define INPUT_BUF_SIZE 80 
 
 char* program;
+
+off_t handle_seek(int fd, off_t offset, int whence) {
+    off_t res_offset = lseek(fd, offset, whence);
+
+    if (res_offset == -1) {
+        printf("%s: error seeking through file\n", program);
+        exit(EXIT_FAILURE);
+    }
+
+    return res_offset;
+}
+
+void handle_null_terminate(char* buffer, int* len) {
+    *len = strlen(input_buf);
+
+    if(*len > INPUT_BUF_SIZE) {
+        // essentially truncate inputted string to match buffer
+        input_buf[INPUT_BUF_SIZE - 1] = '\0';
+        *len = INPUT_BUF_SIZE;
+    } else if (len > 0 && input_buf[len - 1] == '\n') {
+        input_buf[len - 1] = '\0';
+    } 
+}
+
+void handle_write(int fd, char* buffer, ssize_t bytes_requested) {
+    ssize_t total_bytes_written = 0;
+    char* bufw = buffer;
+
+    while(total_bytes_written < bytes_requested) {
+        int bytes_to_write = bytes_requested - total_bytes_written;
+        ssize_t num_written = write(fd, bufw, bytes_to_write);
+
+        if (num_written == -1) {
+            printf("%s: error writing to file\n");
+            exit(EXIT_FAILURE);
+        }
+
+        total_bytes_written += num_written;
+        bufw += num_written;
+    }
+
+    return;
+}
 
 ssize_t handle_read(int fd, char* buffer, ssize_t bytes_requested) {
     ssize_t total_bytes_read = 0;
@@ -18,7 +61,6 @@ ssize_t handle_read(int fd, char* buffer, ssize_t bytes_requested) {
     char* bufr = buffer;
 
     while(total_bytes_read < bytes_requested) {
-        // avoid buffer overflow by always reading <= buf size bytes
         int bytes_to_read = bytes_requested - total_bytes_read; 
         ssize_t num_read = read(fd, bufr, bytes_to_read);
         if (num_read == 0) { // EOF
@@ -35,16 +77,24 @@ ssize_t handle_read(int fd, char* buffer, ssize_t bytes_requested) {
     return total_bytes_read;
 }
 
-void read_byte_input(int* input_store) {
-    int result = scanf(" %d", input_store);
-
-    if (result == 0) return;
-    if (result == -1) {
-        printf("Read error or EOF received, exiting process.\n");
+void handle_input(char* buffer, int num_char, FILE *stream) {
+    if (fgets(buffer, num_char, stream) == NULL) {
+        printf("%s: error reading input\n", program);
         exit(EXIT_FAILURE);
     }
+}
 
-    return;
+long handle_numeric_input(char* buffer, int num_char, FILE *stream) {
+    handle_input(buffer, num_char, stream);
+
+    char* endptr;
+    long value;
+    value = strtol(buffer, &endptr, 10);
+    if (endptr == buffer) { // unable to parse to long
+        return 0; 
+    }
+
+    return value;
 }
 
 const char* prompt = "Option (r for read, w for write, s for seek): ";
@@ -72,62 +122,69 @@ int main(int argc, char** argv[]) {
        exit(EXIT_FAILURE);
     }
 
-    // variables for file processing
-    ssize_t file_read; 
-    char* bufr = (char *) malloc(READ_BUF_SIZE);  
-    char* bufw = (char *) malloc(WRITE_BUF_SIZE);
-    if (bufr == NULL || bufw == NULL) {
+    // buffers for file processing
+    char* read_buf = (char *) malloc(READ_BUF_SIZE);  
+    char* input_buf = (char *) malloc(INPUT_BUF_SIZE);
+    if (read_buf == NULL || input_buf == NULL) {
         printf("%s: error allocating buffer\n", program);
         exit(EXIT_FAILURE);
     }
 
-    int bytes_requested;
-    int byte_cursor;
+    long bytes_to_read;
+    long offset;
+    long whence;
 
     // process user input and interact with file
     while (1) {
         printf("%s", prompt);
-        int option = getchar();
-
+        if(fgets(input_buf, INPUT_BUF_SIZE, stdin) == NULL) {
+            printf("%s: error reading input\n", program);
+            exit(EXIT_FAILURE);
+        }
+        
+        // only accept the first character as the option
+        int option = input_buf[0]; 
         switch(option) {
             case 'r':
                 printf("Enter the number of bytes you want to read: ");
 
-                read_byte_input(&bytes_requested);                
-                // TODO: do I have to make this +=?
-                byte_cursor = handle_read(fd, bufr, bytes_requested);
+                bytes_to_read = handle_numeric_input(input_buf, INPUT_BUF_SIZE, stdin); 
+
+                ssize_t num_read = handle_read(fd, read_buf, bytes_to_read);
                 // only print number of bytes read into buffer 
-                printf("%.*s\n", (int) byte_cursor, bufr);
+                printf("%.*s\n", (int) num_read, read_buf);
                 break;
             case 'w':
-                // handle_write();
                 printf("Enter the data you want to write: ");
-                // if(fgets(bufw, WRITE_BUF_SIZE, stdin) == NULL) {
- //                    printf("%s: error reading user input or EOF reached.\n", program);
-               //  }
-                handle_read(STDIN_FILENO, bufw, WRITE_BUF_SIZE);
-                printf("%s", bufw);
+                handle_input(input_buf, INPUT_BUF_SIZE, stdin);
+
+                size_t len;
+                handle_null_terminate(input_buf, &len);
+                size_t len = strlen(input_buf);
+                if (len > 0 && input_buf[len - 1] == '\n') {
+                    input_buf[len - 1] = '\0';
+                }
+
+                handle_write(fd, input_buf, len);
                 break;
             case 's':
-                // handle_seek();
-                printf("Enter an offset value: \n");
+                printf("Enter an offset value: ");
+                offset = handle_numeric_input(input_buf, INPUT_BUF_SIZE, stdin);      
                 printf("Enter a value for whence: ");
+                whence = handle_numeric_input(input_buf, INPUT_BUF_SIZE, stdin);
+                if (whence < 0 || whence > 3) break;
+                
+                off_t file_offset = handle_seek(fd, offset, whence);
                 break;
             default:
                 // do nothing in this instance
                 break;
         }
-
-        // Source: internet search "getchar ignore newline"
-        // Noticed initially that my prompt was printing multiple times 
-        // because newline character was being read on subsequent iteration 
-        // placed at end of loop bc provided executable re-prints prompt with leading whitespace
-        while((option = getchar()) != '\n' && option != EOF);
+        
     }
 
     // cleanup
-    free(bufr);
-    free(bufw);
+    free(read_buf);
+    free(input_buf);
     close(fd);
 }
-
