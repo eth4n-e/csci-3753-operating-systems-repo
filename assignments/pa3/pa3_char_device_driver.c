cfdee45e6a50eaca5b1cf5a2e46b7debc14ea3a0
eth4n-e <etep9497@colorado.edu>
2025-09-27 09:45:29 -0600

    more work on pa3 + recitation demo

--- assignments/pa3/pa3_char_device_driver.c
+++ assignments/pa3/pa3_char_device_driver.c
@@ -25,6 +25,7 @@ MODULE_LICENSE("GPL");
 static int open_count = 0;
 static int close_count = 0;
 static char *device_buffer;
+const int zero_idx_buf_size = BUFFER_SIZE - 1; // [0, 899] - last index of buf
 
 ssize_t pa3_char_driver_read(struct file *pfile, char __user *buffer,
                              size_t length, loff_t *offset) {
@@ -35,24 +36,32 @@ ssize_t pa3_char_driver_read(struct file *pfile, char __user *buffer,
   /* offset will be set to current position of the opened file after read*/
   /* copy_to_user function: source is device_buffer and destination is the
    * userspace buffer *buffer */
-  ssize_t total_bytes_copied;
+  size_t bytes_available;
   ssize_t bytes_to_copy;
+  unsigned long uncopied_bytes;
+  ssize_t total_bytes_copied;
 
-  if (length + *offset > BUFFER_SIZE) {
-    printk("reading %d bytes will overflow the buffer\n", length + *offset);
-    bytes_to_copy = BUFFER_SIZE - *offset;
-  } else {
+  if (*offset >= BUFFER_SIZE) { // EOF, no bytes to copy
+    return 0;
+  }
+  bytes_available = BUFFER_SIZE - (size_t)*offset;
+
+  if (length < bytes_available) {
     bytes_to_copy = length;
+  } else { // attempt to read more than buffer
+    printk(KERN_ALERT "%s: reading %lld bytes will overflow the buffer\n",
+           DEVICE_NAME, length + *offset);
+    bytes_to_copy = bytes_available;
   }
 
   // copy_to_user returns # bytes failed to copy to user
   // copying can fail for # of reasons -> typically issue with user space buf ->
   // no retry in kernel
-  unsigned long uncopied_bytes =
-      copy_to_user(buffer, device_buffer, bytes_to_copy);
+  uncopied_bytes = copy_to_user(buffer, device_buffer, bytes_to_copy);
   total_bytes_copied = bytes_to_copy - uncopied_bytes;
 
-  printk(KERN_ALERT "%zd bytes read from device\n", total_bytes_copied);
+  printk(KERN_ALERT "%s: %zd bytes read from device\n", DEVICE_NAME,
+         total_bytes_copied);
 
   *offset += total_bytes_copied;
 
@@ -68,26 +77,81 @@ ssize_t pa3_char_driver_write(struct file *pfile, const char __user *buffer,
   /* current position of the opened file*/
   /* copy_from_user function: destination is device_buffer and source is the
    * userspace buffer *buffer */
+  size_t bytes_available;
+  ssize_t bytes_to_copy;
+  unsigned long uncopied_bytes;
+  ssize_t total_bytes_copied;
 
-  return length;
+  if (*offset >= BUFFER_SIZE) { // EOF, no bytes to copy
+    return 0;
+  }
+  bytes_available = BUFFER_SIZE - (size_t)*offset;
+
+  if (length < bytes_available) {
+    bytes_to_copy = length;
+  } else { // write request would cause buffer overflow
+    printk(KERN_ALERT "%s: writing %ld bytes will overflow the buffer\n",
+           DEVICE_NAME, length);
+    bytes_to_copy = bytes_available;
+  }
+
+  uncopied_bytes = copy_from_user(device_buffer, buffer, bytes_to_copy);
+  total_bytes_copied = bytes_to_copy - uncopied_bytes;
+
+  printk(KERN_ALERT "%s: %zd bytes written to device\n", DEVICE_NAME,
+         total_bytes_copied);
+
+  *offset += total_bytes_copied;
+
+  return total_bytes_copied;
 }
 
 int pa3_char_driver_open(struct inode *pinode, struct file *pfile) {
   /* print to the log file that the device is opened and also print the number
    * of times this device has been opened until now*/
-  printk(KERN_ALERT "Device opened. Open count: %d\n", ++open_count);
+  printk(KERN_ALERT "%s: Device has now been opened %d times\n", DEVICE_NAME,
+         ++open_count);
   return 0;
 }
 
 int pa3_char_driver_close(struct inode *pinode, struct file *pfile) {
   /* print to the log file that the device is closed and also print the number
    * of times this device has been closed until now*/
-  printk(KERN_ALERT "Device closed. Close count: %d\n", ++close_count);
+  printk(KERN_ALERT "%s: Device has now been closed %d times\n", DEVICE_NAME,
+         ++close_count);
   return 0;
 }
 
 loff_t pa3_char_driver_seek(struct file *pfile, loff_t offset, int whence) {
   /* Update open file position according to the values of offset and whence */
+  loff_t curr_off = pfile->f_pos;
+  loff_t new_off;
+
+  // custom behavior depending on whence
+  switch (whence) {
+  case 0: // start
+    new_off = offset;
+    break;
+  case 1: // current
+    new_off = offset + curr_off;
+    break;
+  case 2: // end
+    new_off = offset + BUFFER_SIZE;
+    break;
+  default: // only considering whence 0-2
+    return -EINVAL;
+    break;
+  }
+
+  // constant bound checking for all seek strategies
+  if (new_off < 0) { // prevent underflow
+    new_off = 0;
+  } else if (new_off > BUFFER_SIZE) { // prevent overflow
+    new_off = BUFFER_SIZE;
+  }
+
+  pfile->f_pos = new_off;
+  printk(KERN_ALERT "%s: device now at position %lld\n", DEVICE_NAME, new_off);
   return 0;
 }
 
@@ -112,8 +176,7 @@ struct file_operations pa3_char_driver_file_operations = {
 static int pa3_char_driver_init(void) {
   /* print to the log file that the init function is called.*/
   /* register the device */
-  printk(KERN_ALERT "inside %s function\n", __FUNCTION__);
-  // TODO: do I need to check for errors here / with exit func
+  printk(KERN_ALERT "%s: inside %s function\n", DEVICE_NAME, __FUNCTION__);
   device_buffer = (char *)kmalloc(BUFFER_SIZE, GFP_KERNEL);
   if (!device_buffer) {
     // kmalloc returns null on failure
@@ -129,9 +192,12 @@ static int pa3_char_driver_init(void) {
 static void pa3_char_driver_exit(void) {
   /* print to the log file that the exit function is called.*/
   /* unregister the device using the unregister_chrdev() function. */
-  printk(KERN_ALERT "inside %s function\n", __FUNCTION__);
+  printk(KERN_ALERT "%s: inside %s function\n", DEVICE_NAME, __FUNCTION__);
   unregister_chrdev(MAJOR_NUM, DEVICE_NAME);
   kfree(device_buffer);
+  // source:
+  // https://stackoverflow.com/questions/1879550/should-one-really-set-pointers-to-null-after-freeing-them
+  device_buffer = NULL;
 }
 
 /* add module_init and module_exit to point to the corresponding init and exit
